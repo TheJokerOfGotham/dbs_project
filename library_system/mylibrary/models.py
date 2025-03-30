163a702737bc213ac2d167b477d82854911d3879
Kumar Abhishek <kumar.abhishek.krab@gmail.com>
2025-03-30 22:15:43 +0530

    lib settings commit

--- library_system/mylibrary/models.py
+++ library_system/mylibrary/models.py
@@ -19,12 +19,13 @@ class Book(models.Model):
     
 
 class Librarian(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)  # Allow null values
     librarian_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15)
     address = models.TextField()
-    membership_date = models.DateField()
+    membership_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.name
